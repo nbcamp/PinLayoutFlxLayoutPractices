12cf5aad0fc89c49d834b46cc9ac150e93ef5b19
jinyongp <dev.jinyongp@gmail.com>
2023-10-21 22:23:00 +0900

    chore: Preview 사용법 작성

--- PinLayoutFlxLayoutPractices/Controllers/UIViewController+Preview.swift
+++ PinLayoutFlxLayoutPractices/Controllers/UIViewController+Preview.swift
@@ -15,4 +15,18 @@ extension UIViewController {
     }
 }
 
+// [사용법]
+// 아래 코드를 복사한 뒤 미리 보고 싶은 UIViewController가 있는 파일에 배치합니다.
+// [UIViewController] 부분을 보고 싶은 UIViewController 타입 이름으로 변경합니다.
+
+//#if DEBUG
+//import SwiftUI
+//
+//struct [UIViewController]Preview: PreviewProvider {
+//    static var previews: some View {
+//        [UIViewController]().preview()
+//    }
+//}
+//#endif
+
 #endif
--- PinLayoutFlxLayoutPractices/Views/UIView+Preview.swift
+++ PinLayoutFlxLayoutPractices/Views/UIView+Preview.swift
@@ -7,13 +7,24 @@ extension UIView {
 
         func makeUIView(context: Context) -> UIView { view }
 
-        func updateUIView(_ view: UIView, context: Context) {
-//            view.setContentHuggingPriority(.defaultHigh, for: .horizontal)
-//            view.setContentHuggingPriority(.defaultHigh, for: .vertical)
-        }
+        func updateUIView(_ view: UIView, context: Context) {}
     }
 
     var preview: some View { UIViewPreview(view: self) }
 }
+
+// [사용법]
+// 아래 코드를 복사한 뒤 미리 보고 싶은 UIView가 있는 파일에 배치합니다.
+// [UIView] 부분을 보고 싶은 UIView 타입 이름으로 변경합니다.
+
+// #if canImport(SwiftUI) && DEBUG
+// import SwiftUI
+//
+// struct [UIView]Preview: PreviewProvider {
+//    static var previews: some View {
+//        [UIView]().preview
+//    }
+// }
+// #endif
 
 #endif
